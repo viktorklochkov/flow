c0867cf465ceb97d711627da93159d24b8f4a334
Lukas <lukas.kreis@gmail.com>
2018-01-18 13:42:56 +0100

    split project into separate libraries
    split project into separate libraries

---
+++ Base/BaseLinkDef.h
@@ -0,0 +1,30 @@
+//
+// Created by Lukas Kreis on 12.06.17.
+//
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class Qn::Axis+;
+#pragma link C++ class Qn::DataVector+;
+#pragma link C++ class vector<Qn::DataVector >+;
+#pragma link C++ class vector<vector <Qn::DataVector> >+;
+#pragma link C++ class vector<QnCorrectionsQnVector >+;
+#pragma link C++ class vector<Qn::Axis >+;
+#pragma link C++ class Qn::QVec+;
+#pragma link C++ class Qn::QVector+;
+#pragma link C++ class Qn::Profile+;
+#pragma link C++ class Qn::DataContainer<QnCorrectionsQnVector >+;
+#pragma link C++ class Qn::DataContainer<vector<Qn::DataVector> >+;
+#pragma link C++ class Qn::DataContainer<float >+;
+#pragma link C++ class Qn::DataContainer<Qn::Profile>+;
+#pragma link C++ class Qn::DataContainer<std::vector<float> >+;
+#pragma link C++ class Qn::DataContainer<Qn::QVector >+;
+#pragma link C++ typedef Qn::DataContainerProfile;
+#pragma link C++ typedef Qn::DataContainerQVector;
+#pragma link C++ typedef Qn::DataContainerDataVector;
+
+#endif
---
+++ DifferentialCorrection/DiffCorrLinkDef.h
@@ -0,0 +1,11 @@
+//
+// Created by Lukas Kreis on 12.06.17.
+//
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#endif
