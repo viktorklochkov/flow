1ba10fbd1a9d811395ce9f56635382ba6e9b833a
Lukas <lukas.kreis@gmail.com>
2018-10-19 16:46:35 +0200

    fixed typo

--- cmake/QnConfig.cmake
+++ cmake/QnConfig.cmake
@@ -1,3 +1,3 @@
 include(CMakeFindDependencyMacro)
-find_depedency(ROOT REQUIRED Core MathCore RIO Hist Tree Net TreePlayer)
+find_dependency(ROOT COMPONENTS Core MathCore RIO Hist Tree Net TreePlayer)
 include("${CMAKE_CURRENT_LIST_DIR}/QnTargets.cmake")
